0210ff6ac5dfe1c6e1daf5494a0ec6af9592b61e
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 21:02:22 +0530

    3C_inequality.cpp

--- 3C_inequality.cpp
+++ 3C_inequality.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
+ //Mukesh Rothe //23070123089 //CDS EXP3
+ #include<iostream>
  using namespace std;
  int main()
 {
     int a,b;
-    cout<<"enter first number: ";
+    cout<<"Enter first number: ";
     cin>>a;
-    cout<<"enter second number: ";
+    cout<<"Enter second number: ";
     cin>>b;
     cout<<"a!=b: "<<(a!=b)<<endl;
     return 0;
