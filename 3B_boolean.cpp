cc677d2e8cc65230c9d465f61a3fa591fb4442b8
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 21:00:14 +0530

    3B_boolean.cpp

--- 3B_boolean.cpp
+++ 3B_boolean.cpp
@@ -1,7 +1,10 @@
+//Mukesh Rothe //23070123089 //CDS EXP3
 #include<iostream>
 using namespace std;
 int main(){
     bool a= true;
     bool b= false;
-    cout<<boolalpha<<(!b);
+    cout<<boolalpha;
+    cout<<"a&&b: "<<(a&&b)<<endl;
+    return 0;
 }
